39c4dead22c43478b3ceec10b6eb41d84bcadf41
Yana1972 <yana.khokhla@code.berlin>
2023-04-10 16:36:11 +0200

    Code refactoring

--- scripts.js
+++ scripts.js
@@ -1,34 +1,53 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+    if (lockBoard) return;
     this.classList.add('flip');
 
     //First time player click card
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
-    } else {
+      return;
+    }
 
     //Second time player click card
         hasFlippedCard = false;
         secondCard = this;
 
+        checkForMatch();
+}
+
+function checkForMatch(){
     //Do cards match?
-    if (firstCard.dataset.image === secondCard.dataset.image) {
-    //It is a match!
+    let isMatch = firstCard.dataset.image === secondCard.dataset.image;
+    
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCads(){
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
-    }else{
-    //Not a match!
-    setTimeout(() => {
+}
+
+function unflipCards(){
+    lockBoard = true;
+
+setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
-    }, 1500);
-    }
-    }
+    
+    lockBoard = false;
+    }, 1200);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
